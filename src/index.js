c2fa1792fc8adac8370c9031fa019f88616a29f9
codeph-0bia <codephobia642@gmail.com>
2021-07-11 01:06:44 +0530

    removed bootstrap

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.css';
 
 import Counter from './components/counter';
 
